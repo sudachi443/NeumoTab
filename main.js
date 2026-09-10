34338d47a2e3b3c36c67a2f18450ef731d24dd4f
yuke8 <yuke8.sub@gmail.com>
2026-09-10 21:10:26 +0900

    change:create button from engine.json

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
 const searchInput = document.getElementById("search-form");
-
-const button = document.acti
+const engines = document.getElementById("engine");
 
 searchInput.addEventListener('input', () => {
   const keyword = searchInput.value;
   console.log(keyword);
 });
-function ddg() {
-  const keyword = searchInput.value;
-  window.location.href ="https://duckduckgo.com/?q=" + encodeURIComponent(keyword);
+async function getengine() {
+  const data = await fetch("engine.json").then(i => i.json());
+  for (const engine of data) {
+    const button = document.createElement("button");
+    button.className = "searchengine";
+    button.type = "button";
+    button.innerHTML = `
+        <svg class="pointer" width="10" height="10">
+            <circle cx="3" cy="3" r="3" fill="blue" />
+        </svg>
+        ${engine.name}
+    `;
+    button.addEventListener("click", () => {
+        const keyword = searchInput.value;
+        const url = engine.link + encodeURIComponent(keyword);
+        window.location.href = url;
+    });
+    console.log(button);
+    engines.appendChild(button);
+  };
 };
-function chatgpt() {
-  const keyword = searchInput.value;
-  window.location.href ="https://chatgpt.com/?q=" + encodeURIComponent(keyword);
-};
-function twitter() {
-  const keyword = searchInput.value;
-  if(keyword=="") { 
-    window.location.href ="https://x.com/home" + encodeURIComponent(keyword);
-  } else {
-    window.location.href ="https://x.com/search?q=" + encodeURIComponent(keyword);
-  }
-};
-function niconico() {
-  const keyword = searchInput.value;
-  if(keyword=="") { 
-    window.location.href ="https://nicovideo.jp/my" + encodeURIComponent(keyword);
-  } else {
-    window.location.href ="https://nicovideo.jp/search/" + encodeURIComponent(keyword);
-  }
-};
+getengine();
