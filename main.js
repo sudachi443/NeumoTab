880200a4872ff651cdb2474d73f9e5d014aa7f3d
yuke8 <yuke8.sub@gmail.com>
2026-09-11 00:09:54 +0900

    feat:create shortcut from shortcut.json

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const searchInput = document.getElementById("search-form");
 const engines = document.getElementById("engine");
-
+const shortcuts = document.getElementById("dashboard");
 searchInput.addEventListener('input', () => {
   const keyword = searchInput.value;
   console.log(keyword);
@@ -22,8 +22,36 @@ async function getengine() {
         const url = engine.link + encodeURIComponent(keyword);
         window.location.href = url;
     });
-    console.log(button);
     engines.appendChild(button);
   };
 };
+async function getshortcut() {
+  const MAX_LENGTH = 10;
+  const data = await fetch("shortcut.json").then(i => i.json());
+  var len = Object.keys(data).length;
+  for (let i = 0; i < len; i++){
+    const button = document.createElement("button");
+    const shortcut = data[i];
+    if (i == 0){
+      button.id = "fs";
+    } else{
+      button.className = "shortcut";
+    }
+    button.type = "button";
+    const string = shortcut.title;
+    const title = string.length > MAX_LENGTH ? string.slice(0, MAX_LENGTH) + "..." : string;
+    button.innerHTML = `
+        <img src="https://www.google.com/s2/favicons?domain=${encodeURIComponent(shortcut.uri)}" alt="${title}">
+        <span>${title}</span>
+    `;
+    button.addEventListener("click", () => {
+        const url = shortcut.uri;
+        window.location.href = url;
+    });
+    console.log(button);
+    shortcuts.appendChild(button);
+  };
+};
 getengine();
+getshortcut();
+
