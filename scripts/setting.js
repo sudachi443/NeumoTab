86c6098440e6267dd631e291c12a85b9eaded49d
yuke8 <yuke8.sub@gmail.com>
2026-09-16 19:34:10 +0900

    feat:add export feature

--- scripts/setting.js
+++ scripts/setting.js
@@ -24,8 +24,25 @@ async function load_file(id,key){
     document.getElementById(key).textContent = "read error";
   }
   });
-}
+};
+async function export_file(id,key){
+  document.getElementById(id).addEventListener('click',async () => {
+  const json = await load_storage(key);
+  const data = JSON.stringify(json);
+  const blob = new Blob([data], { type: 'application/json' });
+  let dummy_a_el = document.createElement('a');
+  document.body.appendChild(dummy_a_el);
+  dummy_a_el.href = window.URL.createObjectURL(blob);
+  dummy_a_el.download = key+'.json';
+  dummy_a_el.click();
+  document.body.removeChild(dummy_a_el);
+  console.log(data);
+  console.log(key+".json is saved");
+  });
+};
 
+
+export_file("export_shortcut","shortcuts");
 load_file("import_shortcut","shortcuts");
+export_file("export_engine","engines");
 load_file("import_engine","engines");
-
